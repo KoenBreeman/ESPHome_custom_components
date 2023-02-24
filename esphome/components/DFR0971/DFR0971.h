e1744ebcaa9328c529513c61725163c8f163e47c
Koen Breeman <koenbreeman@kpnplanet.nl>
2023-02-24 17:02:14 +0100

    update

--- esphome/components/DFR0971/DFR0971.h
+++ esphome/components/DFR0971/DFR0971.h
@@ -1,1 +1,44 @@
 #pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/output/float-output.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/i2c/i2c.h"
+
+#define GP8302_CONFIG_CURRENT_REG 0x02
+#define OUTPUT_RANGE 0X01
+
+namespace esphome {
+namespace DFR0971 {
+/**
+ * @enum eOutPutRange_t
+ * @brief Analog voltage output range select
+ */
+enum class eOutPutRange_t {
+  eOutputRange5V = 0X00,
+  eOutputRange10V = 0X11,
+};
+
+enum class ChannelSelect : uint8_t {
+			Channel1 = 0,
+			Channel2 = 1,
+			Both = 2
+		}
+
+class DFR0971 : public i2c::I2CDevice, public Component {
+ public:
+  void setup() override;
+  void dump_config() override;
+};
+
+class DFR0971Output : public output::output, public Component, public Parented<DFR0971> {
+ public:
+  DFR0971Output(ChannelBit channel_) : channel(channel_) {}
+
+  void set_max_power(float max_power) override;
+  void set_level(float state) override;
+
+ protected:
+  ChannelSelect channel{ChannelSelect::Both};
+  uint16_t voltage = 0;
+}  // namespace DFR0971
+}  // namespace esphome
--- esphome/components/DFR0971/__init__.py
+++ esphome/components/DFR0971/__init__.py
@@ -1,1 +1,29 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import automation
+from esphome.automation import maybe_simple_id
+from esphome.const import (
+    CONF_ID,
+    CONF_INVERTED,
+    CONF_LEVEL,
+    CONF_MAX_POWER,
+    CONF_MIN_POWER,
+)
+from esphome.core import CORE
+
 CODEOWNERS = ["@Koen Breeman"]
+
+DFR0971_ns = cg.esphome_ns.namespace("DFR0971")
+DFR0971 = DFR0971_ns.class_("DFR0971", cg.Component)
+Channel = DFR0971.class_("Channel", output.FloatOutput)
+
+IS_PLATFORM_COMPONENT = True
+
+CONF_ZERO_MEANS_ZERO = "zero_means_zero"
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(BP5758D),
+        cv.Required(CONF_MAX_POWER): cv.int_range(min=0, max=10),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
