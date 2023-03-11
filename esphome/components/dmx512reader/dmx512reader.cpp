25ec597390555690f0c69f5da7e88e9c63bd91c4
Koen Breeman <koenbreeman@kpnplanet.nl>
2023-03-12 00:35:24 +0100

    added log for number of bytes available

--- esphome/components/dmx512reader/dmx512reader.cpp
+++ esphome/components/dmx512reader/dmx512reader.cpp
@@ -2,56 +2,57 @@
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace dmx512reader {
+	namespace dmx512reader {
 
-static const char *TAG = "dmx512reader";
+		static const char* TAG = "dmx512reader";
 
-void DMX512Reader::loop() {
-  bool update = false;
-  // An update needs to be triggered periodically, if the
-  // periodic update option is set
-  if ( (((millis() - this->last_update_) > this->update_interval_) && this->periodic_update_)) {
-      ESP_LOGD(TAG, "Update UART read array");
-    // Force the refresh rate to be within the spec
-    if ((millis() - this->last_update_) > DMX_MIN_INTERVAL_MS) {
-      update = true;
-    }
-  }
-  if (update) {
-    if (!this->available()) {
-      return;
-    }
-    if (!(this->read_array(this->device_values_, this->max_chan_ + 1))) {
-      
-      ESP_LOGW(TAG, "Junk on wire. Throwing away partial message");
-    }
-    this->last_update_ = millis();
-  }
-}
+		void DMX512Reader::loop() {
+			bool update = false;
+			// An update needs to be triggered periodically, if the
+			// periodic update option is set
+			if ((((millis() - this->last_update_) > this->update_interval_) && this->periodic_update_)) {
+				//ESP_LOGD(TAG, "Update UART read array");
+			  // Force the refresh rate to be within the spec
+				if ((millis() - this->last_update_) > DMX_MIN_INTERVAL_MS) {
+					update = true;
+				}
+			}
+			if (update) {
+				ESP_LOGD(TAG, "number of bytes available: %d", this->available());
+				if (!this->available()) {
 
-void DMX512Reader::dump_config() { ESP_LOGCONFIG(TAG, "Setting up DMX512 Reader..."); }
+					return;
+				}
+				if (!(this->read_array(this->device_values_, this->max_chan_ + 1))) {
 
-void DMX512Reader::setup() {
-  for (int i = 0; i < DMX_MSG_SIZE; i++)
-    this->device_values_[i] = 0;
-  if (this->pin_enable_) {
-    ESP_LOGD(TAG, "Enabling RS485 module");
-    this->pin_enable_->setup();
-    this->pin_enable_->digital_read();
-  }
-}
+					ESP_LOGW(TAG, "Junk on wire. Throwing away partial message");
+				}
+				else
+				{
+					ESP_LOGD(TAG, "Device value 20: %u", device_values_[20]);
+				}
+				this->last_update_ = millis();
+			}
+		}
 
-void DMX512Sensor::setup() {}
+		void DMX512Reader::dump_config() { ESP_LOGCONFIG(TAG, "Setting up DMX512 Reader..."); }
 
-void DMX512Sensor::dump_config() {}
+		void DMX512Reader::setup() {
+			for (int i = 0; i < DMX_MSG_SIZE; i++)
+				this->device_values_[i] = 0;
+		}
 
-void DMX512Sensor::update() { 
-  uint8_t sensorvalue = this->universe_->device_values_[this->channel_];
-  //ESP_LOGD(TAG, "Channel '%fV': value '%fV'", this->channel_, sensorvalue);
-  this->dmxsensor_->publish_state((float)sensorvalue);
+		void DMX512Sensor::setup() {}
 
-}
+		void DMX512Sensor::dump_config() {}
 
-float DMX512Sensor::get_setup_priority() const { return setup_priority::AFTER_CONNECTION; }
-}  // namespace dmx512
+		void DMX512Sensor::update() {
+			uint8_t sensorvalue = this->universe_->device_values_[this->channel_];
+			ESP_LOGD(TAG, "Channel '%f': value '%f'", this->channel_, sensorvalue);
+			this->dmxsensor_->publish_state((float)sensorvalue);
+
+		}
+
+		float DMX512Sensor::get_setup_priority() const { return setup_priority::AFTER_CONNECTION; }
+	}  // namespace dmx512
 }  // namespace esphome
