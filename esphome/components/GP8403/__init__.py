38299d4ef74b742f40d99b3b3db48f214e8ecc93
Koen Breeman <koenbreeman@kpnplanet.nl>
2023-03-22 19:33:35 +0100

    cleaned up comments

--- esphome/components/GP8403/__init__.py
+++ esphome/components/GP8403/__init__.py
@@ -8,26 +8,19 @@ from esphome.const import (
     CONF_INVERTED,
     CONF_LEVEL,
     CONF_MAX_LEVEL,
-    CONF_MIN_LEVEL,
     CONF_CHANNEL
 )
 from esphome.core import CORE
 
 DEPENDENCIES = ["i2c"]
-#AUTO_LOAD = ["output"]
-#MULTI_CONF = True
+MULTI_CONF = True
 
 CODEOWNERS = ["@Koen Breeman"]
 
 GP8403_ns = cg.esphome_ns.namespace("GP8403")
 GP8403 = GP8403_ns.class_("GP8403", cg.Component)
-#Channel = GP8403.class_("Channel", output.FloatOutput)
 
-CONF_I2C_ADDR = 0x5F#0X26
-
-#IS_PLATFORM_COMPONENT = True
-
-CONF_ZERO_MEANS_ZERO = "zero_means_zero"
+CONF_I2C_ADDR = 0x5F
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -44,4 +37,3 @@ async def to_code(config):
     cg.add(var.set_channel(config[CONF_CHANNEL]))
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
-    #return var
